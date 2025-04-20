0ce0b2a8543ac4b755e4d76615bc1453bc911d80
BalakeKarbon <blakekarbon@gmail.com>
2025-04-20 04:44:43 +0000

    Restructured...

--- src/logic.rs
+++ src/logic.rs
@@ -1,4 +1,4 @@
-enum LogicType {
+pub enum LogicType {
     AND,
     OR,
     NAND,
@@ -7,39 +7,62 @@ enum LogicType {
     NOT,
 }
 
-struct LogicElement {
-    value: bool,
-    element: LogicType,
-    inputs: Vec<u32>, 
+struct Logic {
+    state: bool,
+    processor: fn(&Vec<usize>) -> bool,
+    inputs: Vec<usize>,
 }
 
-impl LogicElement {
-
+impl Logic {
+    fn process(&self) -> bool {
+        (self.processor)(&self.inputs)
+    }
 }
 
-pub struct LogicGraph {
-    elements: Vec<LogicElement>,
+struct Sensor {
+    state: bool,
 }
 
-impl LogicGraph {
-    pub fn new() -> Self {
-        LogicGraph {
-            elements: Vec::new(),
-        }
+impl Sensor {
+    fn process(&self) -> bool {
+        self.state // This perhaps could be defined as a function like the other logic in which
+                   // case it could be seen as something like a reader, perhaps a bit more dynamic.
     }
-    pub fn add_element(&mut self) {
-        self.elements.push(LogicElement {
-            value: false,
-            element: LogicType::NAND,
-            inputs: Vec::new(),
-        });
-        println!("Add {}!",self.elements.len());
+    fn set_state(&mut self, state: bool) { // Should this be a reference to the bool?
+        self.state = state;
     }
-    pub fn remove_element(&self) {
-        println!("Remove!");
+}
+
+enum Element {
+    Logic(Logic),
+    Sensor(Sensor),
+}
+
+pub struct Network {
+    elements: Vec<Element>,
+}
+
+impl Network {
+    fn add_logic(&mut self, element_type: LogicType, inputs: &Vec<usize>) -> usize {
+        1
     }
-    pub fn size(&self) -> usize {
+    fn add_sensor(&mut self) -> usize {
+        self.elements.push(Element::Sensor(Sensor {
+            state: false,
+        }));
         self.elements.len()
     }
-}
+    fn remove_element(&mut self, index: usize) { // Perhaps return effected elements or a truth value?
 
+    }
+    fn get_element_state(&self, index: usize) -> Option<bool> {
+        if let Some(element) = self.elements.get(index) {
+            match element {
+                Element::Logic(logic) => Some(logic.state),
+                Element::Sensor(sensor) => Some(sensor.state),
+            }
+        } else {
+            None
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,5 @@
 mod logic;
 
 fn main() {
-    let mut test: crate::logic::LogicGraph = crate::logic::LogicGraph::new();
-    println!("Initial Size: {}",test.size());
-    test.add_element();
-    test.add_element();
-    test.add_element();
-    test.remove_element();
+
 }
