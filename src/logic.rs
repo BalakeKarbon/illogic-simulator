1ca8e078d1fe898f99028a0a41a30bcd26f4a38a
BalakeKarbon <blakekarbon@gmail.com>
2025-04-21 00:52:07 +0000

    Just added some WIP UNFINISHED structure to logic.rs

--- src/logic.rs
+++ src/logic.rs
@@ -7,6 +7,32 @@ pub enum LogicType {
     NOT,
 }
 
+fn And(inputs: &Vec<usize>) -> bool {
+    // AND all input bools.
+    inputs.iter().all(|&b| b) // Check to make sure this works bro. Alternative method using fold 
+                              // would be: inputs.iter().fold(true, |acc, &b| acc && b). For OR the
+                              // fold method would be: index.iter().fold(false, |acc, &b| acc || b);
+}
+fn Or(inputs: &Vec<usize>) -> bool {
+    inputs.iter().any(|&b| b)
+}
+fn Nand(inputs: &Vec<usize>) -> bool {
+    !inputs.iter().all(|&b| b)
+}
+fn Nor(inputs: &Vec<usize>) -> bool {
+    !inputs.iter().any(|&b| b)
+}
+fn Xor(inputs: &Vec<usize>) -> bool {
+    inputs.iter().fold(false, |acc, &b| acc ^ b)
+}
+/* fn Not(inputs: &Vec<usize>) -> bool { // Do we want a vector of inputs? I mean its only practical
+                                      // with one input so if we keep the vector we must decide how
+                                      // we want to process that like an OR of all of them and then
+                                      // an invert of that which is just a NOR...???? or we do a
+                                      // seperate one with just one value...?
+    !inputs.iter().any(|&b| b)
+} */
+
 struct Logic {
     state: bool,
     processor: fn(&Vec<usize>) -> bool,
@@ -44,7 +70,54 @@ pub struct Network {
 
 impl Network {
     fn add_logic(&mut self, element_type: LogicType, inputs: &Vec<usize>) -> usize {
-        1
+        match element_type {
+            LogicType::AND => {
+                self.elements.push(Element::Sensor(Sensor {
+                    state: false,
+                    processor: And(inputs),
+                    inputs: inputs, // This is going to throw an error for a copy or a reference
+                                    // yeah...
+                }));
+            }
+            LogicType::OR => {
+                self.elements.push(Element::Sensor(Sensor {
+                    state: false,
+                    processor: Or(inputs),
+                    inputs: inputs,
+                }));
+            }
+            LogicType::NAND => {
+                self.elements.push(Element::Sensor(Sensor {
+                    state: false,
+                    processor: Nand(inputs),
+                    inputs: inputs,
+                }));
+            }
+            LogicType::NOR => {
+                self.elements.push(Element::Sensor(Sensor {
+                    state: false,
+                    processor: Nor(inputs),
+                    inputs: inputs,
+                }));
+            }
+            LogicType::XOR => {
+                self.elements.push(Element::Sensor(Sensor {
+                    state: false,
+                    processor: Xor(inputs),
+                    inputs: inputs,
+                }));
+            }
+            LogicType::NOT => {
+                self.elements.push(Element::Sensor(Sensor {
+                    state: false,
+                    processor: Nor(inputs), // Should this structure be different because only one
+                                            // input is practical for a not? Then perhaps it would
+                                            // not allow people to do crasy stuff?
+                    inputs: inputs,
+                }));
+            }
+        }
+        self.elements.len()
     }
     fn add_sensor(&mut self) -> usize {
         self.elements.push(Element::Sensor(Sensor {
